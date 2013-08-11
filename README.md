b74a4d3efa29797b5a32053d400b2f43275bc21b
wangheng <king_wangheng@163.com>
2013-08-11 22:11:12 +0800

    Add subprocess_trace.py script.

---
+++ README.md
@@ -0,0 +1,13 @@
+## Copyright (c) 2013, Heng.Wang. All rights reserved.
+## 
+## Some code in my spare time.
+##   
+## @Author: Heng.Wang
+## @Date  : 08/05/2013
+## @Email : wangheng.wh@alibaba-inc.com
+##          king_wangheng@163.com
+##          wangheng.king@gmail.com          
+## @Github: https://github.com/HengWang/
+## @Blog  : http://hengwang.blog.chinaunix.net
+
+All of the code is written in my spare time.
---
+++ python/subprocess_trace.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#****************************************************************#
+# ScriptName: trace.py
+# Author: $SHTERM_REAL_USER@alibaba-inc.com
+# Create Date: 2013-08-09 21:55
+# Modify Author: $SHTERM_REAL_USER@alibaba-inc.com
+# Modify Date: 2013-08-09 21:55
+# Function: 
+#***************************************************************#
+
+
+import os
+import sys
+import resource
+from ptrace.binding import (ptrace_syscall,ptrace_attach,ptrace_getregs)
+from ptrace.syscall import SYSCALL_NAMES
+
+def wait_status():
+    status = None
+    pid, status = os.waitpid(-1,0)
+    if os.WIFEXITED(status):
+        print "-- child process was exited."
+        return -1
+    if os.WIFSIGNALED(status):
+        print "-- child process terminated by a signal."
+        return -1
+    if os.WCOREDUMP(status):
+        print "-- child process coredump."
+        return -1
+    if os.WSTOPSIG(status):
+        return 0
+    return -1
+
+def trace(pid):
+
+    ptrace_attach(pid)
+    if wait_status() == -1:
+        return -1
+    print "-- start traceing %d ..." %pid
+
+    while True:
+        ptrace_syscall(pid)
+        if wait_status() == -1:
+            ptrace_detach(pid)
+            return -1
+        regs = ptrace_getregs(pid)
+        res = SYSCALL_NAMES.get(regs.orig_rax)
+        if res == "clone" or res == "fork" or res == "vfork" or res == "execve":
+            limit = resource.getrlimit(resource.RLIMIT_NPROC)
+            if regs.rax > 0 and regs.rax < limit[1]:
+                print "create new child: %s" %regs.rax
+    return 0
+
+
+if __name__ == "__main__":
+    ppid = int(sys.argv[1])
+    trace(ppid)
+
